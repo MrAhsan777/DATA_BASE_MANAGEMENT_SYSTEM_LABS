58c5fb887834f4f6ba44e79e728eb314708e35de
Muhammad Ahsan <150921311+MrAhsan777@users.noreply.github.com>
2024-11-03 13:49:30 +0500

    Update ex1 (1).sql

--- DBMS-LAB-01/ex1 (1).sql
+++ DBMS-LAB-01/ex1 (1).sql
@@ -15,8 +15,8 @@ CREATE TABLE "Course"(
 
 INSERT INTO "St_info" ("st_no", "adm_date", "st_name", "st_age", "st_address") 
 VALUES 
-('1', '2024-10-21', 'Abdul Rauf', 21, 'nwb '),
-('2', '2024-10-22', 'Muhammad Ahsan', 22, 'nwb'),
+('1', '2024-10-21', 'Muhammad Ahsan', 21, 'nwb '),
+('2', '2024-10-22', 'Waqar Ahmed', 22, 'nwb'),
 ('3', '2024-10-23', 'Muhammad Umar', 23, 'nwb'),
 ('4', '2024-10-24', 'Abdul Rehman', 24, 'shapur'),
 ('5', '2024-10-25', 'Amjad Ali', 25, 'nwb');
